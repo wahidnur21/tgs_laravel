ae3d0fbc85bf6aef2f6389654972f67a4773b306
ihsann <ihsann820@gmail.com>
2024-04-01 15:51:30 +0700

    selesai

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -1,7 +1,7 @@
 
       <nav class="navbar navbar-expand-lg bg-primary">
         <div class="container-fluid">
-          <a class="navbar-brand text-light" href="#">Navbar</a>
+          <a class="navbar-brand text-light" href="#">INW</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
